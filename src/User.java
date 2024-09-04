c650bee026d6ca2f224178b7e7c0a8869c95db82
nawal <nawalali00609@gmail.com>
2024-09-04 18:14:39 +1000

    user

---
+++ src/User.java
@@ -0,0 +1,23 @@
+public class User {
+    //ArrayList<Integer> studentID = ArrayList<>(); //8 characters
+    //public ArrayList<String> alias, pronouns; 
+    String email, password; 
+    Date dateOfBirth; //check if valid 
+
+ }
+    
+    //password reqs, 1 uppercase letter + 1 special character, min 8 char
+    //no repeating aliass
+    //info stored for login?
+
+   /* public studentID() {
+        //add student ID's here
+        //studentID.add(new int(....))
+    }
+
+    public alias() {
+        //add aliases here
+        //alias.add(new String(....))
+    }
+
+}*/
