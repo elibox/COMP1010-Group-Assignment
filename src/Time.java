0c459a808eb96552851c9c42e80622ef62eb3f4f
nawal <nawalali00609@gmail.com>
2024-09-04 18:04:02 +1000

    time

---
+++ src/Time.java
@@ -0,0 +1,13 @@
+public class Time {
+    public int hour, minute, second;
+    
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    
+    public String toString() {
+        return ""+this.hour+":"+this.minute+":"+this.second;
+   } 
+}
