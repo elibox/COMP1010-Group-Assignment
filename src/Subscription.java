47a2fd825e60a18512490ee54d7e2ea530d0e874
caesou <caaesou@gmail.com>
2024-10-05 17:24:23 +1000

    minor change: added small to do list in subscription class

--- src/Subscription.java
+++ src/Subscription.java
@@ -10,8 +10,13 @@ public class Subscription {
     /*message for when user subscribed to channel? or idk if this could just be a printline 
     under a subscribeTo method in user?
 
+    i can add this later... hopefully my code won't be scuffed -- claire
+
     public String toString() {
         return(user.toString()+"has subscribed to"+channel.toString());
     }*/
 
+    /* to do: 
+     - print a list of channels we're subscribed to
+     */
 }
